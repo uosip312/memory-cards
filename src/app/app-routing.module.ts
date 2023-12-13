f0dbc710ecd73e7eafd21f5e324613a0c6682b14
Edison de la Cruz <edison.icr@gmail.com>
2023-12-13 23:11:58 +0100

    feat(app): modulos, rutas y componentes iniciales

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = 
+[
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'game',
+    loadChildren: () => import('./pages/game/game.module').then(m => m.GameModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
